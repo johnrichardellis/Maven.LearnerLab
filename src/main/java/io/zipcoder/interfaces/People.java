8c8295377f9d56769dd0bb12c312b9ecf821f52d
Ellis John <ellisjohn@zipcodembpchuck.myfiosgateway.com>
2021-11-28 14:03:12 -0500

    lab

--- src/main/java/io/zipcoder/interfaces/People.java
+++ src/main/java/io/zipcoder/interfaces/People.java
@@ -3,44 +3,64 @@ package io.zipcoder.interfaces;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class People<E extends Person> implements Iterable<E>{
+public abstract class People<E extends Person> implements Iterable<E> {
 
 
-        List<E> personList;
+    List<E> personList;
 
-        public People() {
+    public People() {
 
-            personList = new ArrayList<E>();
-
-        }
-
-        public void add(E person) {
-
-            personList.add(person);
-
-        }
-
-        public void findById(long id) {
-            // todo
-        }
-
-        public boolean contains(Person person) {
-            if (personList.contains(person)) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        public void remove(Person person) {
-            for (E i: personList) {
-                if (personList[i] != person) {
-                    continue;
-                }
-                personList.remove(person);
-            }
-
-        }
-
+        personList = new ArrayList<E>();
 
     }
+
+    public void add(E person) {
+
+        personList.add(person);
+
+    }
+
+    public void findById(long id) {
+        // todo
+    }
+
+    public boolean contains(Person person) {
+        if (personList.contains(person)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+//    public void remove(Person person) {
+//        for (E i : personList) {
+//            if (person != personList[i]) {
+//                continue;
+//            }
+//            personList.remove(person);
+//        }
+//
+//    }
+
+
+    public int count() {
+
+        return personList.size();
+
+    }
+
+
+    public void removeAll() {
+
+        personList.clear();
+
+    }
+
+//    public E[] toArray() {
+//
+//        // todo
+//        return true;
+//    }
+
+
+}
---
+++ src/test/java/io/zipcoder/interfaces/TestPeople.java
@@ -0,0 +1,15 @@
+package io.zipcoder.interfaces;
+
+import org.junit.Test;
+
+public class TestPeople {
+
+
+
+    @Test
+    public void testAdd() {
+
+    }
+}
+
+
